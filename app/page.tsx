4d4c85d160374b7e02a5e57d7769028c1b99c382
HeinerAcosta <heiner@kru360.com>
2024-12-26 18:08:35 -0500

    config redirect path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+'use client'
+
 import { MainLayout } from "@/components/layout/MainLayout";
+import { useEffect } from "react";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+
+  useEffect(() => {
+    redirect("/event-detail/1");
+  }, []);
+
   return (
     <MainLayout>
       <div></div>
