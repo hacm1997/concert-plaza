9eaadbd6a4f6885e0c166df646f90b031d2b636a
HeinerAcosta <heiner@kru360.com>
2024-12-26 22:10:59 -0500

    build fixed

--- app/event-detail/[id]/page.tsx
+++ app/event-detail/[id]/page.tsx
@@ -1,23 +1,22 @@
 'use client'
 
-// import { Banner } from "@/components/event-detail-components/Banner";
-// import { MainHeading } from "@/components/event-detail-components/MainHeading";
+import { Banner } from "@/components/event-detail-components/Banner";
+import { MainHeading } from "@/components/event-detail-components/MainHeading";
 import { MainLayout } from "@/components/layout/MainLayout";
-// import { useConcert } from "@/hooks/useConcert";
+import { useConcert } from "@/hooks/useConcert";
 
 
 export default function Page() {
-  // const concertData = useConcert();
+  const concertData = useConcert();
 
   return (
     <MainLayout>
-      <div></div>
-      {/*concertData &&
+      {concertData &&
         <>
           <Banner banner_img={concertData && concertData.banner_img} name={concertData?.name} description={concertData?.description} dates_places={concertData?.dates_places ?? []} />
           <MainHeading />
         </>
-      */}
+      }
     </MainLayout>
   );
 };
